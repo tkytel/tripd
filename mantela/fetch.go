85807ade7b9d809a6e21c8a8b68cf90e2a6c4d5f
yude <i@yude.jp>
2025-06-08 13:36:22 +0900

    Support mantela written in data URI

--- mantela/fetch.go
+++ mantela/fetch.go
@@ -2,19 +2,38 @@ package mantela
 
 import (
 	"crypto/tls"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"io"
 	"net/http"
+	"strings"
 )
 
-func FetchMantela(mantelaUrl string) (Mantela, error) {
+func FetchMantela(mantelaUri string) (Mantela, error) {
+	// if mantelaUrl is data uri, parse it directly
+	if strings.Contains(mantelaUri, "data") {
+		b := strings.TrimPrefix(mantelaUri, `data:application/json;base64,`)
+		j, err := base64.StdEncoding.DecodeString(b)
+		if err != nil {
+			return Mantela{}, err
+		}
+
+		var m Mantela
+		if err := json.Unmarshal(j, &m); err != nil {
+			return Mantela{}, err
+		}
+
+		return m, nil
+	}
+
+	// if mantelaUri is truly uri, retrieve / parse it
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	client := &http.Client{Transport: tr}
 
-	resp, err := client.Get(mantelaUrl)
+	resp, err := client.Get(mantelaUri)
 	if err != nil {
 		return Mantela{}, err
 	}
